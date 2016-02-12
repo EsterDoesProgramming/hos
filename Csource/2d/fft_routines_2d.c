d4f41aefbf29de0822239bf1a23f038cb5b50323
beisiegel <nicole.beisiegel@gmx.de>
2016-02-12 15:40:02 +0000

    Added documentation

--- Csource/2d/fft_routines_2d.c
+++ Csource/2d/fft_routines_2d.c
@@ -1,13 +1,22 @@
-//
-//  fft_routines.c
-//  euler
-//
-//  Created by Claudio Viotti on 3/7/13.
-//  Copyright (c) 2013 Claudio Viotti. All rights reserved.
-//
+/**
+ * @file fft_routines.c
+ * @brief Provides FFT routines for Euler equations
+ *
+ * @author Claudio Viotti 
+ * @date 3/7/13.
+ * @note Copyright (c) 2013 Claudio Viotti. All rights reserved.
+ */
 
 #include "fft_routines_2d.h"
 
+
+/**
+ * @brief FFT routine in two dimensions
+ *
+ * @param u a double array pointer that contains the function values
+ * @param hu a fftw_complex array pointer that contains the Fourier coefficients (afterwards)
+ * @param plan a fftw_plan data structure
+ */
 
 void fft_2d(double* u, fftw_complex* hu, fftw_plan plan){
     
@@ -35,6 +44,7 @@ void fft_2d(double* u, fftw_complex* hu, fftw_plan plan){
             
             index=(Ny/2+1)*i + j;
             
+            /* FFTW3 does not include the normalization of the Fourier coefficients */
             hu[index]=hf[index]/Nx/Ny;
             
         }
@@ -43,6 +53,14 @@ void fft_2d(double* u, fftw_complex* hu, fftw_plan plan){
     
 }
 
+
+/**
+ * @brief IFFT routine in two dimensions
+ *
+ * @param hu a fftw_complex array pointer that contains the Fourier coefficients (afterwards)
+ * @param u a double array pointer that contains the function values
+ * @param plan a fftw_plan data structure
+ */
 
 void ifft_2d(fftw_complex* hu, double* u, fftw_plan plan){
     
@@ -79,6 +97,100 @@ void ifft_2d(fftw_complex* hu, double* u, fftw_plan plan){
     
 }
 
+/**
+ * @brief FFT routine in two dimensions
+ *
+ * @param u a double array pointer that contains the function values
+ * @param hu a fftw_complex array pointer that contains the Fourier coefficients (afterwards)
+ * @param plan a fftw_plan data structure
+ */
+
+void fft_2d(double* u, fftw_complex* hu, fftw_plan plan){
+    
+    int index;
+    
+    for (int i=0; i<Nx; i++) {
+        
+        for (int j=0; j<Ny; j++) {
+            
+            index=Ny*i + j;
+    
+            f[index]=u[index];
+            
+        }
+        
+    }
+    
+    
+    fftw_execute(plan);
+    
+    
+    for (int i=0; i<Nx; i++) {
+    
+        for (int j=0; j<Ny/2+1; j++) {
+            
+            index=(Ny/2+1)*i + j;
+            
+            /* FFTW3 does not include the normalization of the Fourier coefficients */
+            hu[index]=hf[index]/Nx/Ny;
+            
+        }
+        
+    }
+    
+}
+
+
+/**
+ * @brief IFFT routine in two dimensions
+ *
+ * @param hu a fftw_complex array pointer that contains the Fourier coefficients (afterwards)
+ * @param u a double array pointer that contains the function values
+ * @param plan a fftw_plan data structure
+ */
+
+void ifft_2d(fftw_complex* hu, double* u, fftw_plan plan){
+    
+    int index;
+    
+    for (int i=0; i<Nx; i++) {
+        
+        for (int j=0; j<Ny/2+1; j++) {
+            
+            index=(Ny/2+1)*i + j;
+            
+            hf[index]=hu[index];
+            
+        }
+        
+    }
+
+    
+    fftw_execute(plan);
+    
+    
+    for (int i=0; i<Nx; i++) {
+        
+        for (int j=0; j<Ny; j++) {
+            
+            index=Ny*i + j;
+            
+            u[index]=f[index];
+            u[index]=u[index];
+            
+        }
+        
+    }
+    
+}
+
+/**
+ * @brief FFT routine in two dimensions for large arrays
+ *
+ * @param u a double array pointer that contains the function values
+ * @param hu a fftw_complex array pointer that contains the Fourier coefficients (afterwards)
+ * @param plan a fftw_plan data structure
+ */
 
 void fft_2d_large(double* u, fftw_complex* hu, fftw_plan plan){
     
@@ -115,6 +227,13 @@ void fft_2d_large(double* u, fftw_complex* hu, fftw_plan plan){
     
 }
 
+/**
+ * @brief IFFT routine in two dimensions for large arrays
+ *
+ * @param hu a fftw_complex array pointer that contains the Fourier coefficients (afterwards)
+ * @param u a double array pointer that contains the function values
+ * @param plan a fftw_plan data structure
+ */
 
 void ifft_2d_large(fftw_complex* hu, double* u, fftw_plan plan){
     
